b8ecfdb4486a98a29c02701f7d51828442a84160
harokku <crenna.simone@gmail.com>
2018-08-24 17:18:45 +0200

    Added initial database connection

--- models/products.go
+++ models/products.go
@@ -12,6 +12,7 @@ type product struct {
 var productList = []product{
 	{ID: "CLP", Name: "CLIPS", ForeignName: "CLIPS hot galvanized"},
 	{ID: "CN0017", Name: "Concertina", ForeignName: "Barbed Tape Concertina BTO 22 - Profil Ultra - 700mm - HDG Z180"},
+	{ID: " EC0017A", Name: "Lama dentata per nastratrice - singolo bisello - passo 2,5 mm - 3 asole (12x5)", ForeignName: "Tape sealer - pitch 2,5 mm."},
 }
 
 // Return all products
--- sapConnector.go
+++ sapConnector.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"log"
 )
 
+// SqlServer connection parameters
+var server = "localhost"
+var port = 1433
+var user = "sa"
+var password = "your_password"
+
+// Global pointers for Gin router and Sql
 var r *gin.Engine
+var db *sql.DB
 
 func main() {
+	var err error
+
+	// DataBase connection string
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d", server, user, password, port)
+
+	// Create connection pool
+	db, err = sql.Open("SqlServer", connString)
+	if err != nil {
+		log.Fatal("Error creating connection pool: " + err.Error())
+	}
+	log.Println("Connected to database")
+
+	// Defer connection pool close after program exits
+	defer db.Close()
+
+	// TODO: Implement database pinging and version check
+
 	// Router definition
 	r = gin.Default()
 
